93b8ad6d9b55bb03179b01b52384b4cb5ef86e43
Stefan <stefanseifeddine@gmail.com>
2022-09-28 00:18:52 +0100

    Add new font styles

--- webpack.config.js
+++ webpack.config.js
@@ -23,10 +23,14 @@ module.exports = {
     },
     module: {
         rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
     optimization: {
