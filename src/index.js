ec090f246525d087b33384c9b7be17408e844e0a
Stefan <stefanseifeddine@gmail.com>
2022-09-29 17:50:04 +0100

    Delete unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import _ from 'lodash';
 import './style.css';
-import printMe from './print.js';
 import pageLoad from './pageLoad.js'
 import addNewProject from './projectFunction'
 import {addNewProjectCard} from './projectFunction'
 
-function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
-
-    return element;
-  }
-
 addNewProjectCard()
 addNewProject()
-  // document.body.appendChild(component());
